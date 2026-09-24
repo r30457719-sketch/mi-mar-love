2bc7130a8c74c292e38450374523969d921fd7df
r30457719-sketch <r30457719@gmail.com>
2026-09-24 11:34:53 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,10 @@
 function entrar() {
-
     const boton = document.querySelector("button");
 
     boton.innerHTML = "Entrando a nuestro mundo... 🌊";
-
     boton.style.pointerEvents = "none";
 
     setTimeout(() => {
-
-        alert("Aquí comenzará nuestro mundo ❤️");
-
-        boton.innerHTML = 'Entrar a nuestro mundo <span>→</span>';
-
-        boton.style.pointerEvents = "auto";
-
+        window.location.href = "mundo.html";
     }, 1000);
 }
